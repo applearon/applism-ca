65d605d1a536479bf645bd7de14a18d1f5d577a8
applearon <aronseshan@gmail.com>
2024-10-28 08:50:15 -0400

    Add NWC

--- langs.php
+++ langs.php
@@ -12,13 +12,21 @@ $l18n = [
         ],
     ],
     "home" => [
-        "banner" => [
+        "nbtbanner" => [
             "en" => "New Beginnings Tournament 2nd place Banner",
             "fr" => "Bannière 2ème place du New Beginnings Tournament",
         ],
         "nbt_info" => [
             "en" => "This is the first (and only so far!) tournament I've participated in. Despite being the worst team by rank, and ranking as seed 13; we managed to make it to Grand Finals, and got to the Tie Breaker map against the winning team!",
             "fr" => "text alternatif"
+        ],
+        "nwcbanner" => [
+            "en" => "Newcomers World Cup Custom Banner",
+            "fr" => "text alternatif"
+        ],
+        "nwc_info" => [
+            "en" => "As the name implies, this is a World Cup, so naturally I played for the Canada team. We had a great start, ranking as seed 6. Unfortunately, we fell to the losers bracket in the Quarterfinals, to the eventual 2nd place team, France, and finally marking our loss in the Losers Semifinals, to the strong Spain team. Our coach, Samububu, and our team captain, Dimples, were both integral to our success. The Team:",
+            "fr" => "baguette",
         ],
         "title" => [
             "en" => "Applearon's osu! things",
--- main.php
+++ main.php
@@ -1,10 +1,29 @@
 <h1><?php print($l18n['home']['title'][$lang]);?></h1>
 <div class="card">
   <h2>New Beginnings Tournament</h2>
-  <img title="<?php print($l18n['home']['banner'][$lang])?>" alt="<?php print($l18n['home']['banner'][$lang])?>" src="/assets/NBT_Banner.png"></img>
+  <img title="<?php print($l18n['home']['nbtbanner'][$lang])?>" alt="<?php print($l18n['home']['nbtbanner'][$lang])?>" src="/assets/NBT_Banner.png"></img>
   <div class="inner-card">
     <p class="text">
       <?php print($l18n['home']['nbt_info'][$lang]) ?>
+    </p>
+  </div>
+</div>
+<div class="card">
+  <div class="inner-card">
+    <h2>Newcomers World Cup</h2>
+  <img title="<?php print($l18n['home']['nwcbanner'][$lang])?>" alt="<?php print($l18n['home']['nwcbanner'][$lang])?>" src="/assets/NWC_Banner.png"></img>
+
+    <p class="text">
+      <?php print($l18n['home']['nwc_info'][$lang]) ?>
+      <p>
+        <a href="https://osu.ppy.sh/users/22558127">Applearon</a>
+        <a href="https://osu.ppy.sh/users/13348268">DimplesRMe</a>
+        <a href="https://osu.ppy.sh/users/10406176">ImpureConnects</a><br>
+        <a href="https://osu.ppy.sh/users/24900500">Iselle</a>
+        <a href="https://osu.ppy.sh/users/23996416">Sqoop</a>
+        <a href="https://osu.ppy.sh/users/35248251">Torrows</a><br>
+        <a href="https://osu.ppy.sh/users/9554330">Samububu</a>
+      </p>
     </p>
   </div>
 </div>
